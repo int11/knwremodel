574cf74245bb51d7318d605b9da5f4463f02d7f7
kiwi1020 <ryugas2@naver.com>
2023-09-29 20:24:20 +0900

    woo

--- src/main/java/com/kn/knwremodel/controller/MainController.java
+++ src/main/java/com/kn/knwremodel/controller/MainController.java
@@ -19,6 +19,7 @@ public class MainController {
     private final NoticeRepository noticeRepo;
     private final NoticeCrawlerService noticeCrawler;
 
+
     @GetMapping(value="/")
     public String test(Model model) throws IOException{
         noticeCrawler.updata();
