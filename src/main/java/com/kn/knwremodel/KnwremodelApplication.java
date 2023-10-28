3169d6a69b729bf45e27fa832c669f71e84139eb
Eunbin Seo <sso061356@naer.com>
2023-10-28 11:00:31 +0900

    fix haksa update

--- src/main/java/com/kn/knwremodel/KnwremodelApplication.java
+++ src/main/java/com/kn/knwremodel/KnwremodelApplication.java
@@ -2,6 +2,7 @@ package com.kn.knwremodel;
 
 import com.kn.knwremodel.service.CollegeService;
 import com.kn.knwremodel.service.NoticeService;
+import com.kn.knwremodel.service.haksaService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -18,6 +19,7 @@ import java.io.IOException;
 public class KnwremodelApplication {
 	private final NoticeService noticeS;
 	private final CollegeService collegeS;
+	private final haksaService haksaS;
 
 	public static void main(String[] args) {
 		SpringApplication.run(KnwremodelApplication.class, args);
@@ -27,6 +29,13 @@ public class KnwremodelApplication {
 	public void testSchedule() throws IOException {
 		collegeS.testdata();
 		noticeS.updata();
+		haksaS.crawlAndSaveData();
 		System.out.println("DataBase Update every 30 mininutes");
+	}
+
+	@Scheduled(cron = "0 0 1 */6 * ?") // 반기별 1일 0시에 실행
+	public void updateHaksa() throws IOException {
+		haksaS.crawlAndSaveData();
+		System.out.println("Haksa Data Updated every 6 months");
 	}
 }
