0a02ec588f856a5a1bab5a04d9e79d74ec3edd65
dongdongsw <fishman4535@gmail.com>
2023-12-07 15:01:44 +0900

    ----------게시판--------------- 컨트롤러 수정 및 html 수정

--- src/main/java/com/kn/knwremodel/controller/IndexController.java
+++ src/main/java/com/kn/knwremodel/controller/IndexController.java
@@ -2,27 +2,30 @@ package com.kn.knwremodel.controller;
 
 import com.kn.knwremodel.dto.UserDTO;
 import com.kn.knwremodel.entity.Posts;
+import com.kn.knwremodel.repository.PostsRepository;
 import com.kn.knwremodel.service.PostService;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RequiredArgsConstructor
-@RequestMapping("/dev")
-@Controller
+@RestController
 public class IndexController {
     private final HttpSession httpSession;
     private final PostService postService;
-
+    private final PostsRepository postsRepository;
     @GetMapping("/notice")
-    public String index(Model model) {
+    public List<Posts> findAllDesc(Model model) {
         model.addAttribute("posts", postService.findAllDesc());
-        return "index.html";
+        return postsRepository.findAllDesc();
     }
 
-    @GetMapping("/savenotice")
+    @GetMapping("/posts/save")
     public String postsSave(Model model) {
         UserDTO.Session currentuserDTO = (UserDTO.Session) httpSession.getAttribute("user");
         if (currentuserDTO != null) {
@@ -31,19 +34,19 @@ public class IndexController {
         return "posts-save";
     }
 
-    @GetMapping("/update/{id}")
+    @GetMapping("/posts/update/{id}")
     public String postsUpdate(@PathVariable Long id, Model model) {
         Posts post = postService.findById(id);
         model.addAttribute("post", post);
         return "posts-update";
     }
 
-    @PostMapping("/save")
+    @PostMapping("/api/v1/posts")
     public Long save(@RequestBody Posts requestDto) {
         return postService.save(requestDto.getTitle(), requestDto.getContent(), requestDto.getWriter());
     }
 
-    @PutMapping("/update/{id}")
+    @PutMapping("/api/v1/posts/{id}")
     public Long update(@PathVariable Long id, @RequestBody Posts requestDto) {
         return postService.update(id, requestDto.getTitle(), requestDto.getContent());
     }
@@ -53,7 +56,7 @@ public class IndexController {
         return postService.findById(id);
     }
 
-    @DeleteMapping("/delete/{id}")
+    @DeleteMapping("/api/v1/posts/{id}")
     public Long delete(@PathVariable Long id) {
         postService.delete(id);
         return id;
